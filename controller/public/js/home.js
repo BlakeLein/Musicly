13045326d95b8edc4542f83eb2c3dcb11b2941ef
Blake Lein <blake.lein@gmail.com>
2022-08-10 12:55:06 -0500

    Tweaked mobile responsiveness

--- controller/public/js/home.js
+++ controller/public/js/home.js
@@ -6,11 +6,20 @@ signInButton.addEventListener("click", () => {
   window.location.href = "/signin";
 });
 
-function myFunction() {
-  const x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+// function myFunction() {
+//   const x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+// Collapsable NavBar Fucntions
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("menu-button").style.marginLeft = "250px";
+}
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("menu-button").style.marginLeft = "0";
 }
